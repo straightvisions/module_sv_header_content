3808585f44d3b6b10678b6b46ee882e302706d40
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-04-25 11:48:08 +0200

    ADD SPACING SETTINGS

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -20,20 +20,4 @@
 	require_once( $script->get_parent()->get_path( 'lib/frontend/css/config/title.php' ) );
 	require_once( $script->get_parent()->get_path( 'lib/frontend/css/config/excerpt.php' ) );
 
-
-	// Those can be overwritten by metabox settings
-	$header_content_overlay_color = $script->get_parent()->get_header_content_overlay_color();
-?>
-
-.sv100_sv_header_content {
-    background-color: rgba(<?php echo $bg_color; ?>);
-}
-.sv100_sv_header_content_wrapper {
-max-width: <?php echo $wrapper_max_width; ?>;
-}
-.sv100_sv_header_content_content {
-    background-color: rgba(<?php echo $header_content_overlay_color; ?>);
-	margin: <?php echo $script->get_parent()->get_setting('align')->get_data(); ?>;
-	max-width: <?php echo $max_width; ?>;
-	min-height: <?php echo $min_height; ?>;
-}
+	require_once( $script->get_parent()->get_path( 'lib/frontend/css/config/general.php' ) );
---
+++ lib/frontend/css/config/general.php
@@ -0,0 +1,88 @@
+<?php
+
+	$properties					= array();
+
+	// Margin
+	if($margin) {
+		$imploded		= false;
+		foreach($margin as $breakpoint => $val) {
+			$top = (isset($val['top']) && strlen($val['top']) > 0) ? $val['top'] : false;
+			$right = (isset($val['right']) && strlen($val['right']) > 0) ? $val['right'] : false;
+			$bottom = (isset($val['bottom']) && strlen($val['bottom']) > 0) ? $val['bottom'] : false;
+			$left = (isset($val['left']) && strlen($val['left']) > 0) ? $val['left'] : false;
+
+			if($top !== false || $right !== false || $bottom !== false || $left !== false) {
+				$imploded[$breakpoint] = $top . ' ' . $right . ' ' . $bottom . ' ' . $left;
+			}
+		}
+		if($imploded) {
+			$properties['margin'] = $setting->prepare_css_property_responsive($imploded, '', '');
+		}
+	}
+
+	// Padding
+	// @todo: same as margin, refactor to avoid doubled code
+	if($padding) {
+		$imploded		= false;
+		foreach($padding as $breakpoint => $val) {
+			$top = (isset($val['top']) && strlen($val['top']) > 0) ? $val['top'] : false;
+			$right = (isset($val['right']) && strlen($val['right']) > 0) ? $val['right'] : false;
+			$bottom = (isset($val['bottom']) && strlen($val['bottom']) > 0) ? $val['bottom'] : false;
+			$left = (isset($val['left']) && strlen($val['left']) > 0) ? $val['left'] : false;
+
+			if($top !== false || $right !== false || $bottom !== false || $left !== false) {
+				$imploded[$breakpoint] = $top . ' ' . $right . ' ' . $bottom . ' ' . $left;
+			}
+		}
+		if($imploded) {
+			$properties['padding'] = $setting->prepare_css_property_responsive($imploded, '', '');
+		}
+	}
+
+	// border
+	if($border) {
+		if($border['top_width']){
+			$val		= $border['top_width'].' '.$border['top_style'].' rgba('.$border['color'].')';
+			$properties['border-top'] = $setting->prepare_css_property_responsive($val, '', '');
+		}
+		if($border['right_width']){
+			$val		= $border['right_width'].' '.$border['right_style'].' rgba('.$border['color'].')';
+			$properties['border-right'] = $setting->prepare_css_property_responsive($val, '', '');
+		}
+		if($border['bottom_width']){
+			$val		= $border['bottom_width'].' '.$border['bottom_style'].' rgba('.$border['color'].')';
+			$properties['border-bottom'] = $setting->prepare_css_property_responsive($val, '', '');
+		}
+		if($border['left_width']){
+			$val		= $border['left_width'].' '.$border['left_style'].' rgba('.$border['color'].')';
+			$properties['border-left'] = $setting->prepare_css_property_responsive($val, '', '');
+		}
+
+		if($border['top_left_radius']+$border['top_right_radius']+$border['bottom_right_radius']+$border['bottom_left_radius']!==0) {
+			$border_radius = $border['top_left_radius'] . ' ' . $border['top_right_radius'] . ' ' . $border['bottom_right_radius'] . ' ' . $border['bottom_left_radius'];
+			$properties['border-radius'] = $setting->prepare_css_property_responsive($border_radius, '', '');
+		}
+	}
+
+	echo $setting->build_css(
+		'.sv100_sv_header_content',
+		$properties
+	);
+
+
+	// Those can be overwritten by metabox settings
+	$header_content_overlay_color = $script->get_parent()->get_header_content_overlay_color();
+	?>
+
+.sv100_sv_header_content {
+background-color: rgba(<?php echo $bg_color; ?>);
+}
+.sv100_sv_header_content_wrapper {
+max-width: <?php echo $wrapper_max_width; ?>;
+}
+.sv100_sv_header_content_content {
+background-color: rgba(<?php echo $header_content_overlay_color; ?>);
+margin: <?php echo $script->get_parent()->get_setting('align')->get_data(); ?>;
+max-width: <?php echo $max_width; ?>;
+min-height: <?php echo $min_height; ?>;
+}
