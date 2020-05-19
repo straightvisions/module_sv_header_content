ee6b2df6c7263af0b57c9ad788387a4034121641
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-05-19 15:33:39 +0200

    REMOVE MARGIN TOP/BOTTOM FROM ALIGN SETTING

--- sv_header_content.php
+++ sv_header_content.php
@@ -75,9 +75,9 @@
 				->set_title( __( 'Alignment', 'sv100' ) )
 				->set_description( __( 'Set the alignment of the Content-Header', 'sv100' ) )
 				->set_options( array(
-					'10px auto'									=> __( 'center', 'sv100' ),
-					'10px auto 10px 0'								=> __( 'left', 'sv100' ),
-					'10px 0 10px auto'								=> __( 'right', 'sv100' )
+					'0 auto'									=> __( 'center', 'sv100' ),
+					'0 auto 0 0'								=> __( 'left', 'sv100' ),
+					'0 0 0 auto'								=> __( 'right', 'sv100' )
 				) )
 				->set_default_value( '10px auto' )
 				->set_is_responsive(true)
