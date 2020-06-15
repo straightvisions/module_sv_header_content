a53a1e85158a6b2709b58f83728047a3166de63c
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-06-15 15:34:55 +0200

    FEAT - ADD - categories

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -1,8 +1,11 @@
 <div class="<?php echo $this->get_prefix(); ?> <?php echo $this->has_featured_image() ? $this->get_prefix('featured_image') : ''; ?>">
 	<div class="<?php echo $this->get_prefix( 'wrapper' ); ?>">
 		<div class="<?php echo $this->get_prefix( 'content' ); ?>">
+			<?php the_category(); ?>
 			<h1><?php the_title()?></h1>
+			<?php if(get_the_excerpt()){ ?>
 			<div class="<?php echo $this->get_prefix( 'excerpt' ); ?>"><?php the_excerpt(); ?></div>
+			<?php } ?>
 			<?php require( $this->get_path( 'lib/frontend/tpl/meta.php' ) ); ?>
 		</div>
 	</div>
--- lib/frontend/tpl/meta.php
+++ lib/frontend/tpl/meta.php
@@ -1,20 +1,15 @@
 <?php if ( $this->show_meta() ) { ?>
 	<div class="<?php echo $this->get_prefix( 'meta' ); ?>">
 		<?php
-
-		$meta = array();
-		if($this->show_author()){
-			$meta[]		= '<span class="'.$this->get_prefix('author').'">'.get_the_author().'</span>';
-		}
-		if($this->show_date()){
-			$meta[]		= '<span class="'.$this->get_prefix('date').'">'.get_the_date().'</span>';
-		}
-		if($this->show_date_modified()){
-			$meta[]		= '<span class="'.$this->get_prefix('date_modified').'">'.get_the_modified_date().'</span>';
-		}
-
-		echo implode(' - ', $meta);
-
+			if($this->show_author()){
+				echo '<span class="'.$this->get_prefix('author').'">'.get_the_author().'</span>';
+			}
+			if($this->show_date()){
+				echo '<span class="'.$this->get_prefix('date').'">'.get_the_date().'</span>';
+			}
+			if($this->show_date_modified()){
+				echo '<span class="'.$this->get_prefix('date_modified').'">'.get_the_modified_date().'</span>';
+			}
 		?>
 	</div>
 <?php } ?>
