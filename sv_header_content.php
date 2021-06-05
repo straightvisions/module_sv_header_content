741806419b59ebae23c036683ab8bf11b6cadead
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-06-05 13:35:38 +0200

    FIX EXCERPT FONT

--- sv_header_content.php
+++ sv_header_content.php
@@ -217,6 +217,7 @@
 				->set_title( __( 'Font Family', 'sv100' ) )
 				->set_description( __( 'Choose a font for your text.', 'sv100' ) )
 				->set_options( $this->get_module( 'sv_webfontloader' ) ? $this->get_module( 'sv_webfontloader' )->get_font_options() : array('' => __('Please activate module SV Webfontloader for this Feature.', 'sv100')) )
+				->set_is_responsive(true)
 				->load_type( 'select' );
 
 			$this->get_setting( 'font_size_excerpt' )
